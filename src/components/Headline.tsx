79ed584e728b2818dfa328f9099ba373d1d8e1ad
Kens04 <kento090403@icloud.com>
2024-10-12 08:14:43 +0900

    💄 スタイルファイルの分割

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,12 +1,14 @@
+import styles from "../css/styles.module.css";
+
 type Props = {
   page: string;
   children: React.ReactNode;
-}
+};
 
 export function Headline(props: Props) {
   return (
     <div>
-      <h1 className="title">{props.page} page</h1>
+      <h1 className={styles.title}>{props.page} page</h1>
       <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
         <li className="mb-2">Get started by editing {props.children}.</li>
         <li>Save and see your changes instantly.</li>
