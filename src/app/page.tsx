ec7639a6bea3a2c5ac352bd9ba02b9e5b12b664d
Kens04 <kento090403@icloud.com>
2024-10-13 08:57:37 +0900

    ✨ 講座用のuseEffectを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,30 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Main } from "@/components/main";
-import { useCallback } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
+  // const handleClick = useCallback((e: React.MouseEvent<HTMLElement>) => {
+  //   e.preventDefault();
+  //   alert(123);
+  // }, []);
 
-  const handleClick = useCallback((e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    alert(123);
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <a href="/about" onClick={handleClick}>
+      {/* <a href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </a> */}
       <Main page="home" />
       <Footer />
     </div>
