b01a23f30e24c8186beb22a12b30295e957accda
Kens04 <kento090403@icloud.com>
2024-10-12 12:17:57 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書いてみました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,21 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Main } from "@/components/main";
+import { useCallback } from "react";
 
 export default function Home() {
+
+  const handleClick = useCallback((e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    alert(123);
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="home" />
       <Footer />
     </div>
