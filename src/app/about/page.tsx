8b01c9c7b9774cdbd8ffd126574d602773576eca
Kens04 <kento090403@icloud.com>
2024-10-12 09:25:32 +0900

    ✨ ヘッダーの追加

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
 import { Main } from "@/components/main";
 
 export default function About() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Header />
       <Main page="about" />
       <Footer />
     </div>
