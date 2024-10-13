ff01b2b3e04e9e4a0b02676f1797dc3fc81d7e57
Kens04 <kento090403@icloud.com>
2024-10-13 09:13:52 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Main } from "@/components/main";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  // const handleClick = useCallback((e: React.MouseEvent<HTMLElement>) => {
-  //   e.preventDefault();
-  //   alert(123);
-  // }, []);
+  const [count, setCount] = useState(1);
+
+  const handleClick = () => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
     console.log("マウント時");
@@ -24,9 +26,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      {/* <a href="/about" onClick={handleClick}>
-        ボタン
-      </a> */}
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="home" />
       <Footer />
     </div>
