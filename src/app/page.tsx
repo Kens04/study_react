344469a29ff80cb042c873d82bfbfc9161dfd394
Kens04 <kento090403@icloud.com>
2024-10-13 18:26:44 +0900

    ✨ useMemoについて学びました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ import { useCounter } from "@/hooks/useCounter";
 import { useInputArray } from "@/hooks/useInputArray";
 
 export default function Home() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { doubleCount, isShow, handleClick, handleDisplay } = useCounter();
   const { text, array, handleChange, handleAdd } = useInputArray();
   useBgLightBlue();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
